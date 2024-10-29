3c2a79022ed3470ec44931dfe782f7f0efb81084
devluisluzardo <devluisluzardo@gmail.com>
2024-10-29 03:21:29 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function msjini() {
+  // Aquí puedes poner el código que quieres que se ejecute
+  alert("Hola IGLESIA IVS");
+  // Puedes agregar más acciones, como mostrar alertas, modificar el DOM, etc.
+}
