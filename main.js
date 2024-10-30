0d6b3baa4d1ac079c92d8309ba8d1a6677454d35
ivsregionalatlantico <ivsregionalatlantico@gmail.com>
2024-10-30 12:17:26 +0000

    three commit

--- main.js
+++
@@ -1,5 +0,0 @@
-function msjini() {
-  // Aquí puedes poner el código que quieres que se ejecute
-  alert("Hola IGLESIA IVS");
-  // Puedes agregar más acciones, como mostrar alertas, modificar el DOM, etc.
-}
